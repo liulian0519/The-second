f6f80a4d1528a8d197ccf9c3fbe2aca84e0e5078
liulian0519 <1223342263@qq.com>
2017-12-06 19:53:59 +0800

    moogose static

--- new/chap01/app.js
+++ new/chap01/app.js
@@ -1,9 +1,18 @@
 var db = require("./models/db.js");
 //定义了一个模型，学生模型 “学生类”
 var Student = require("./models/Student.js");
-//实例化了一个学生类
-var xiaoming = new Student({"name":"小明","age":12,"sex":'男'});
-//保存
-xiaoming.save(function () {
-    console.log("存储成功");
-});
+// //实例化了一个学生类
+// var xiaoming = new Student({"name":"小明","age":12,"sex":'男'});
+// //保存
+// xiaoming.save(function () {
+//     console.log("存储成功");
+// });
+// Student.create({"name":"liulian","age":20,"sex":"女"},function (error) {
+//     console.log("保存成功");
+// })
+// Student.zhaoren("小明",function (error,result) {
+//     console.log(result);
+// })
+Student.xiugai({"name":"小明"},{$set : {"age":22}},{},function () {
+    console.log("修改成功");
+})
--- new/chap01/models/Student.js
+++ new/chap01/models/Student.js
@@ -6,6 +6,17 @@ var studentSchema = new mongoose.Schema({
     age :{type:Number},
     sex : {type:String}
 });
+//创建静态方法
+studentSchema.statics.zhaoren = function(name,callback){
+    return this.model('Student').find({name:name},callback);
+}
+//创建修改的静态方法
+studentSchema.statics.xiugai = function (conditions,update,options,callback) {
+    this.model("Student").update(conditions,update,options,callback);
+}
+
+
 //创建了一个学生模行，就是学生类
 var studentModel = db.model('Student',studentSchema)
+
 module.exports = studentModel;
