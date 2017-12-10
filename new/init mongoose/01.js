fa2018f8abc95cc074341e91bc12f3d2c9353e2e
liulian0519 <1223342263@qq.com>
2017-12-10 21:42:01 +0800

    init save

--- new/init mongoose/01.js
+++ new/init mongoose/01.js
@@ -1,9 +1,16 @@
-//引包
+﻿//引包
 var mongoose = require("mongoose");
 //链接数据库
 mongoose.connect('mongodb://localhost/haha');
 var Cat = mongoose.model('cat',{name:String});
-var kitty = new Cat({name: 'liulian'});
-kitty.save(function (err) {
-    console.log("miaofj");
-})
+// var kitty = new Cat({name: 'liulian'});
+// kitty.save(function (err) {
+//     console.log("miaofj");
+// })
+Cat.find({"name":"liulian"},function(err,result){
+    var xiaomao = result[0];
+    xiaomao.name = "刘莲";
+    xiaomao.save(function (err) {
+        console.log("fujiv");
+    });
+});
