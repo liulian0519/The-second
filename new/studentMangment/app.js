35bced21f70e769e5148f4a632b043fa128426d8
liulian0519 <1223342263@qq.com>
2017-12-13 17:13:56 +0800

    :art:  showIndex

--- new/studentMangment/app.js
+++ new/studentMangment/app.js
@@ -5,7 +5,7 @@ var db = require("./models/db.js");
 
 
 app.set("view engine","ejs");
-// app.get("/",router.showIndex);
+app.get("/",router.showIndex);
 app.get("/add",router.showAdd);
 app.get("/doAdd",router.doAdd);
 app.listen(3000);
--- new/studentMangment/router/router.js
+++ new/studentMangment/router/router.js
@@ -1,12 +1,26 @@
 var mongoose = require("mongoose");
 var Student = require("../models/Student.js")
+
+//首页
+exports.showIndex = function (req,res,next) {
+    Student.find({},function (err,result) {
+        res.render("index",{
+            "students":result
+        });
+    });
+
+}
+
+
+//显示插入
 exports.showAdd = function (req,res,next) {
     res.render("add");
 }
+//执行插入
 exports.doAdd = function (req,res,next) {
     //存储数据
 Student.create(req.query,function () {
-    console.log("插入成功");
-})
-
+    // console.log("插入成功");
+    res.send("插入成功")
+});
 }
