4e0bbc07d4a17427802272736ca953b585b7245a
liulian0519 <1223342263@qq.com>
2017-12-19 20:24:51 +0800

    emit on

--- new/socket/01.js
+++ new/socket/01.js
@@ -13,6 +13,7 @@ io.on("connection",function (socket) {
     console.log("1个客户端连接了");
     socket.on("tiwen",function (msg) {
         console.log("本服务器得到了一个提问" + msg);
+        socket.emit("huida","我吃了");
     })
 })
 server.listen(3000,"127.0.0.1");
