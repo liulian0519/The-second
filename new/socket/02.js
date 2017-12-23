158259f521db375d029d2f90866de20eca3cc7a4
liulian0519 <1223342263@qq.com>
2017-12-23 09:41:10 +0800

    canvas

---
+++ new/socket/02.js
@@ -0,0 +1,19 @@
+var http = require("http");
+var fs = require("fs");
+var server = http.createServer(function (req,res) {
+   if(req.url == "/"){
+       fs.readFile("./index.html",function (err,data) {
+           res.end(data);
+       });
+   }
+});
+var io = require('socket.io')(server);
+//监听连接事件
+io.on("connection",function (socket) {
+    console.log("1个客户端连接了");
+    socket.on("tiwen",function (msg) {
+        // console.log("本服务器得到了一个提问" + msg);
+       io.emit("huida",msg);
+    })
+})
+server.listen(3000,"127.0.0.1");
